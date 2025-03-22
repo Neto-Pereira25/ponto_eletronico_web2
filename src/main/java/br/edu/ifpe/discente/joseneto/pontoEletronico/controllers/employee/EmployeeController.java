7f1e14e3c2803938d9fa99f6ad5e8a297200ac30
Neto-Pereira25 <devneto203@gmail.com>
2025-03-22 13:29:48 -0300

    Implementado a rota para deletar funcionario

--- src/main/java/br/edu/ifpe/discente/joseneto/pontoEletronico/controllers/employee/EmployeeController.java
+++ src/main/java/br/edu/ifpe/discente/joseneto/pontoEletronico/controllers/employee/EmployeeController.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -85,6 +86,14 @@ public class EmployeeController {
 
     }
 
-    // public ResponseEntity<Employee> Delete() {
-    // }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> Delete(@PathVariable int id) {
+        try {
+            employeeRepository.delete(id);
+            return ResponseEntity.noContent().build();
+        } catch (SQLException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
+                    "Falha ao deletar funcionários");
+        }
+    }
 }

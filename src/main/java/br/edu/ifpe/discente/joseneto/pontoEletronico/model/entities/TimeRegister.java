5ab5d89faaa0f96b99ce8e6656d5615bb31c52c1
Neto-Pereira25 <devneto203@gmail.com>
2025-03-22 00:12:31 -0300

    Criado entidade TimeRegister

--- src/main/java/br/edu/ifpe/discente/joseneto/pontoEletronico/model/entities/TimeRegister.java
+++ src/main/java/br/edu/ifpe/discente/joseneto/pontoEletronico/model/entities/TimeRegister.java
@@ -1,5 +1,35 @@
 package br.edu.ifpe.discente.joseneto.pontoEletronico.model.entities;
 
+import java.time.LocalDateTime;
+
 public class TimeRegister {
+
+    private Integer id;
+    private Integer employeeId;
+    private LocalDateTime dateTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Integer employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
 
 }
